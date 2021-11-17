1ea51372ff599ee7e0e014f7578deeafa810b233
Diego Br <88857178+DiegoBr7@users.noreply.github.com>
2021-11-17 17:40:16 -0300

    Add files via upload

---
+++ desestruturandoobj.js
@@ -0,0 +1,21 @@
+var aluno = {
+    matricula: 1234,
+    nome: "marcos",
+    telefone: 987654432,
+    cidade:"barra do pirai"
+}
+
+var aluno1 = {
+    matricula: 1235,
+    nome: "joao",
+    telefone: 987654789,
+    cidade:"barra do pirai"
+}
+var alunos = [aluno1 , aluno]
+
+console.log(alunos)
+
+var [marcos, joao] = alunos
+
+console.log(marcos)
+ 
